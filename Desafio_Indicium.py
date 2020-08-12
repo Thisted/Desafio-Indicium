66c9147f2923f1e5fcc3d1c3d85e91d516646f1f
Ricardo Thisted <ricardo.thisted@gmail.com>
2020-08-12 02:59:09 -0300

    Update Desafio_Indicium.py

--- Desafio_Indicium.py
+++ Desafio_Indicium.py
@@ -1,18 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+#Importing Libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 # ## Extract
-
-# In[2]:
-
 
 #Loading DataFrames
 contato = pd.read_csv(r"contacts.tsv", sep = "\t", usecols=[' contactsId', 'contactsName'])
@@ -23,17 +15,10 @@ setor = pd.read_csv(r"sectors.tsv", sep = "\t")
 
 # ## Transform
 
-# In[3]:
-
-
 #Modifing Columns Names;
 contato.columns = ['contactsId', 'contactsName']
 compania.columns = ['companiesId', 'companiesName', 'contactsId', 
                     'contactsName','sectorKey']
-
-
-# In[4]:
-
 
 #Creating and Merging the Final DateFrame;
 df_final = contato.copy()
@@ -42,10 +27,6 @@ df_final = df_final.merge(venda, on=['contactsId', 'companiesId'])
 print('Numero de linhas excluidas= ' + str(df_final.isnull().sum().sum()))
 df_final.dropna(axis=0)
 df_final.head(5)
-
-
-# In[5]:
-
 
 #Transforming Dates and Sectors
 from datetime import datetime
@@ -62,18 +43,13 @@ for i in df_final['sectorKey']:
     
 df_final['dealsDateCreated'] = dates
 df_final['sectorKey'] = setores
-
 df_final.head(5)
 
 
 # ## Output 1
 
-# In[6]:
-
-
 output1 = df_final[['contactsName', 'dealsPrice']]
 output1 = output1.groupby(output1['contactsName']).sum()
-
 #Plotting results 1;
 fig, ax = plt.subplots(figsize=(15,8))
 output1.plot(ax=ax, kind='bar')
@@ -83,14 +59,10 @@ plt.savefig('graph_1.png')
 plt.show()
 
 
-# In[7]:
-
-
 output2 = df_final[['dealsDateCreated', 'dealsPrice']]
 output2['dealsDateCreated'] = pd.to_datetime(output2['dealsDateCreated'], format='%Y-%m').dt.strftime('%Y-%m')
 output2.sort_values(['dealsDateCreated'], inplace=True)
 output2 = output2.groupby(output2['dealsDateCreated']).sum()
-
 #Plotting Result 2;
 fig, ax = plt.subplots(figsize=(15,7))
 output2.plot(ax=ax, kind='bar')
@@ -102,13 +74,8 @@ plt.show()
 
 # ## Output 2
 
-# In[8]:
-
-
-#Output 3:
 output3 = df_final.groupby(['companiesName', 'sectorKey']).agg({'dealsPrice': 'sum'})
 output3 = output3.groupby(level=0).apply(lambda x: 100 * x / float(x.sum()))
-
 output3.to_csv('output_2.csv')
 
 
